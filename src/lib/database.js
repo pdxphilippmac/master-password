1e11864dedb3360c5ed0c7ee371d1e53e0599ca9
Philipp Maecke <p.maecke@gmx.de>
2019-10-04 17:10:35 +0200

    add mongodb remote db

--- src/lib/database.js
+++ src/lib/database.js
@@ -27,7 +27,8 @@ const { MongoClient } = require("mongodb");
 let db = null;
 async function initDatabase() {
   //Connection url:
-  const url = "mongodb://localhost:27017/master-password";
+  const url =
+    "mongodb+srv://admin:neuefische@masterpassword-c3tl2.mongodb.net/admin?retryWrites=true&w=majority";
   // Database name:
   const dbName = "master-password";
 
